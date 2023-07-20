ce31d6ed9ecac4c57f549e8e8222ca3a39dea0b2
Ayomide <120118911+Ayo-folashade@users.noreply.github.com>
2023-07-20 15:10:18 +0100

    Initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 from dotenv import load_dotenv, find_dotenv
 from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
 from langchain.document_loaders import CSVLoader
 from langchain.embeddings import OpenAIEmbeddings, openai
 from langchain.prompts import PromptTemplate
@@ -14,17 +14,19 @@ openai.api_key = os.environ['OPENAI_API_KEY']
 
 def setup_chain():
     # Define file path and template
-    file = 'job_skills.csv'
-    template = """You are a language model AI developed to provide support and information on job search topics only. \
-            Your objective is to provide accurate and helpful responses to a wide range of job search questions. \
-            In your responses, ensure a tone of professionalism, expertise, and encouragement. \
+    file = 'job_skill.csv'
+    template = """You are a language model AI developed for a job opportunity project. \
+            You are a friendly chat buddy designed to provide support and information on \
+            job opportunities. Your objective is to provide accurate and empathetic responses to a wide range of \
+            job opportunity questions, based on the 'job_skills.csv' specified in file. \
 
             Here are some specific interaction scenarios to guide your responses:
             - If the user asks what you can do, respond with "I'm a chat buddy  here to provide \
             support and information on job search. How can I assist you?"
             - If the user starts with a greeting, respond with 'Hello! How can I help you with your job search today?' \
             or something related to that
-            - If a user poses a job search-related question, answer the question based on the CSV dataset. \
+            - Your responses should be summarized as much as possible
+            - If a user poses a job opportunity-related question, answer the question based on the CSV dataset. \
             If the exact question is not available, provide a response based on job search topics.
             - If a user asks a question unrelated to job search, respond with \
             'This question is out of my scope as I'm built mainly to help support you with job search-related \
@@ -46,7 +48,8 @@ def setup_chain():
     chain_type_kwargs = {"prompt": prompt}
 
     # Initialize ChatOpenAI
-    llm = ChatOpenAI(
+    llm = OpenAI(
+        model_name="text-davinci-003",
         temperature=0
     )
 
